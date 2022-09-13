5a720d2a8f467839e99f58e60a1bf0b19131a19f
ryancollins-dev <email@ryd3v.rocks>
2022-09-13 03:23:24 -0400

    add structs

---
+++ contracts/ex8_structs.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+// 2022-09-13
+// @ryd3v
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract MyContract {
+  mapping(uint256 => bool) someMapping;
+  mapping(address => uint256) balanceOfAddress;
+
+  struct MyStruct {
+    uint256 timestamp;
+    uint256 counter;
+  }
+
+  mapping(address => MyStruct) public myMapping;
+
+  function myFunction() public {
+    myMapping[msg.sender].timestamp = block.timestamp;
+    myMapping[msg.sender].counter++;
+  }
+}
