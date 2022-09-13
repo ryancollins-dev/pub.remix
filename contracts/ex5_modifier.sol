566d4575e5575249e38125845af305c8f176d306
ryancollins-dev <email@ryd3v.rocks>
2022-09-13 02:29:21 -0400

    update

---
+++ contracts/ex5_modifier.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+// 2022-09-13
+// @ryd3v
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract MyContract {
+  uint256 public myVarOne;
+  uint256 public myVarTwo;
+  bool writeable;
+
+  modifier mustBeWriteable() {
+    require(writeable);
+    _;
+  }
+
+  function setWriteable(bool _writeable) public {
+    writeable = _writeable;
+  }
+
+  function updateMyVarOne(uint256 _myVar) public {
+    myVarOne = _myVar;
+  }
+
+  function updateMyVarTwo(uint256 _myVar) public {
+    myVarTwo = _myVar;
+  }
+}
