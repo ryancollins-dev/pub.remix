8be085441f6774246ee892fa943302f3d45ed2f9
ryancollins-dev <email@ryd3v.rocks>
2022-09-13 04:28:59 -0400

    add params

--- contracts/Notary.sol
+++ contracts/Notary.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 // 2022-09-13
 // @ryd3v
+// params: 0x9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08, "test", "test"
 
 pragma solidity >=0.7.0 <0.9.0;
 
