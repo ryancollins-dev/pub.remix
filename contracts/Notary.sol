5b7c21ac15996b4480e69dd9c58e9969551145eb
Ryan Collins <email@ryd3v.rocks>
2022-10-06 04:28:36 -0400

    add simple storage

--- contracts/Notary.sol
+++ contracts/Notary.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 // 2022-09-13
-// @ryd3v
+// @ryd3v.eth
 
 pragma solidity >=0.7.0 <0.9.0;
 
--- contracts/ex4_constructor.sol
+++ contracts/ex4_constructor.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
+// @ryd3v.eth
 
 pragma solidity >=0.7.0 <0.9.0;
 
